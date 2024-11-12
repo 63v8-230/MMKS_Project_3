7f8e71075c72f8966d18bc06ce524b9b1cc2d95a
K_Ina <zuikaku1939y@gmail.com>
2024-11-12 16:05:38 +0900

    よわいAIを追加

---
+++ Assets/Scripts/Player/AIPlayerBase.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AIPlayerBase : MonoBehaviour, IPlayer //AIのベースクラス
+{
+    [SerializeField]
+    private ETeam _team;
+
+    [SerializeField]
+    protected GameObject stone;
+
+    public Deck MyDeck { set; get; }
+
+    public ETeam Team { set => _team = value; get => _team; }
+
+    public OnlineScript OnlineScriptRef { get; set; }
+
+    protected GameManager gameManager;
+
+    async public virtual Task<TurnInfo> DoTurn()
+    {
+        await Task.Delay(500);
+        return new TurnInfo();
+    }
+
+    public virtual void Init(GameManager gManager)
+    {
+        gameManager = gManager;
+    }
+
+    protected bool IsCorner(int x, int y, Vector2 size)
+    {
+        return (x == 0 || x == (int)size.x) && (y == 0  || y == (int)size.y);
+    }
+
+    protected bool IsEdge(int x, int y, Vector2 size)
+    {
+        return x == 0 || x == (int)size.x || y == 0 || y == (int)size.y;
+    }
+}
---
+++ Assets/Scripts/Player/AIPlayerS.cs
@@ -0,0 +1,219 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Mathematics;
+using UnityEngine;
+using static UnityEngine.InputManagerEntry;
+
+public class AIPlayerS : AIPlayerBase
+{
+
+    private int currentTurn = 0;
+
+    async public override Task<TurnInfo> DoTurn()
+    {
+        currentTurn++;
+        await Task.Delay(500);
+        var puttablePosition = gameManager.StoneManagerRef.GetPuttablePosition(Team);
+        Debug.Log("Puttable: " + puttablePosition.Length);
+        TurnInfo t = new TurnInfo();
+        t.X = -1;
+
+        Task<TurnInfo>[] checkTasks =
+        {
+            Check(IsCorner,puttablePosition),
+            Check(IsArrow,puttablePosition),
+            Check(IsEdge,puttablePosition),
+            Check(IsShield,puttablePosition),
+            Check(IsCircle,puttablePosition),
+            Check(IsCrystal,puttablePosition),
+            Check(IsX,puttablePosition),
+            Check(IsCross,puttablePosition),
+            Check(IsSun,puttablePosition),
+
+        };
+
+        var ts = Task.WhenAll(checkTasks);
+
+        while (!ts.IsCompleted)
+        {
+            await Task.Delay(1);
+        }
+
+        for(int i = 0; i<checkTasks.Length; i++)
+        {
+            if (checkTasks[i].Result.X !=-1)
+            {
+                var kind = GetStone(i);
+                Debug.Log(kind.ToString());
+                bool isEmpty = false;
+                for(int i2 = 0; i2 < MyDeck.Stones.Count; i2++)
+                {
+                    if (MyDeck.Stones[i2].Stone == kind && kind!=EStone.DEFAULT)
+                    {
+                        if (MyDeck.Stones[i2].Amount > 0)
+                        {
+                            var d = MyDeck.Stones[i2];
+                            d.Amount--;
+                            MyDeck.Stones[i2] = d;
+                            Debug.Log("Used");
+                            break;
+                        }
+                        else
+                        {
+                            Debug.Log("Empty");
+                            isEmpty = true;
+                            break;
+                        }
+                    }
+                        
+                }
+                if (isEmpty)
+                    continue;
+
+                t = checkTasks[i].Result;
+                t.PutStone = gameManager.StoneManagerRef.SelectStone(GetStone(i));
+                t.PutStone.GameObjectRef.transform.Find("Plane").localPosition = new Vector3(0, 0.086f, 0);
+                if ((int)kind > 1)
+                    (t.PutStone as SkillStoneBase).IsOwnerOnline = true;
+
+                break;
+            }
+        }
+
+        Debug.Log("=X: " + t.X);
+
+        if (t.X == -1 && puttablePosition.Length > 0) 
+        {
+
+            var pp = puttablePosition[UnityEngine.Random.Range(0, puttablePosition.Length)];
+            t.X = pp.X;
+            t.Y = pp.Y;
+            t.PutStone = gameManager.StoneManagerRef.SelectStone(EStone.DEFAULT);
+        }else if(puttablePosition.Length == 0)
+        {
+            t.X = -1;
+            return t;
+        }
+
+        t.PutStone.SetTeam(Team);
+
+        return t;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Deck d = new Deck();
+        d.Stones = new List<OwnStone>();
+        d.Stones.Add(new OwnStone { Stone = EStone.SUN, Amount = 1 });
+        d.Stones.Add(new OwnStone { Stone = EStone.CROSS, Amount = 1 });
+        d.Stones.Add(new OwnStone { Stone = EStone.X, Amount = 1 });
+        d.Stones.Add(new OwnStone { Stone = EStone.CRYSTAL, Amount = 1 });
+        d.Stones.Add(new OwnStone { Stone = EStone.CIRCLE, Amount = 1 });
+        d.Stones.Add(new OwnStone { Stone = EStone.SHIELD, Amount = 1 });
+        d.Stones.Add(new OwnStone { Stone = EStone.ARROW, Amount = 1 });
+
+        MyDeck = d;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private async Task<TurnInfo> Check(Func<int, int, Vector2, bool> f, PuttableCellInfo[] p)
+    {
+        TurnInfo turn = new TurnInfo();
+        await Task.Delay(1);
+        foreach (var item in p)
+        {
+            if (f(item.X, item.Y, gameManager.StoneManagerRef.GetBoardSize()))
+            {
+
+                turn.X = item.X;
+                turn.Y = item.Y;
+                return turn;
+            }
+        }
+        turn.X = -1;
+        return turn;
+    }
+
+    private EStone GetStone(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                return EStone.DEFAULT;
+            case 1:
+                return EStone.ARROW;
+            case 2:
+                return EStone.DEFAULT;
+            case 3:
+                return EStone.SHIELD;
+            case 4:
+                return EStone.CIRCLE;
+            case 5:
+                return EStone.CRYSTAL;
+            case 6:
+                return EStone.X;
+            case 7:
+                return EStone.CROSS;
+            case 8:
+                return EStone.SUN;
+            default:
+                return EStone.DEFAULT;
+        }
+    }
+
+    private bool IsArrow(int x, int y, Vector2 size)
+    {
+        if (Team == ETeam.WHITE)
+        {
+            return y < 3;
+        }
+        else
+        {
+            return y >= size.y - 4;
+        }
+    }
+
+    private bool IsShield(int x, int y, Vector2 size)
+    {
+        return (x == 1 || x == 2 || x == size.x - 2 || x == size.x - 3) &&
+            (y == 1 || y == 2 || y == size.y - 2 || y == size.y - 3);
+    }
+
+    private bool IsCircle(int x, int y, Vector2 size)
+    {
+        return ((x == 1 || x == 3 || x == size.x - 2 || x == size.x - 4) && (y == 2 || y == size.y - 3)) ||
+            ((x == 2 || x == size.x - 3) && (y == 3));
+    }
+
+    private bool IsCrystal(int x, int y, Vector2 size)
+    {
+        return ((x == 2 || x == size.x - 3) && (y == size.y - 4)) ||
+            ((x == 3 || x == size.x - 4) && (y == 3 || y == size.y - 2));
+    }
+
+    private bool IsX(int x, int y, Vector2 size)
+    {
+        return ((x == 2 || x == size.x - 3) || (y == 3 || y == size.y - 4)) ||
+            ((x == 3 || x == size.y - 4) && (y == 2 || y == size.y - 3));
+    }
+
+    private bool IsCross(int x, int y, Vector2 size)
+    {
+        return (x == 1 || x == 3 || x == -4 || x == -2) && (y == 1 || y == size.y - 2);
+    }
+
+    private bool IsSun(int x, int y, Vector2 size)
+    {
+        return ((x == 2 || x == size.x - 3) && (y == 2 || y == size.y - 3)) ||
+            ((x == 3) && (y == 3)) || ((x == size.x - 4) && (y == size.y - 4));
+    }
+}
