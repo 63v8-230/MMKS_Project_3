667fdacd72d084a10d48984ad3c1370d2a27925b
K_Ina <zuikaku1939y@gmail.com>
2024-11-13 22:03:06 +0900

    ひっくる帰ったときの演出

--- Assets/Scripts/Board/StoneLight.cs
+++ Assets/Scripts/Board/StoneLight.cs
@@ -11,17 +11,37 @@ public class StoneLight : MonoBehaviour
 
     float f = 3;
 
+
+    /// <summary>
+    /// <para>0: Normal</para>
+    /// <para>1: Skill</para>
+    /// </summary>
+    int kind = -1;
+
     // Update is called once per frame
     void Update()
     {
         t -= Time.deltaTime;
-        //var c = m.color;
-        //c -= cl * Time.deltaTime * f;
-        //m.color = c;
 
-        var rc = r.color;
-        rc.a = t * 3;
-        r.color = rc;
+        switch (kind)
+        {
+            case 0:
+                var c = m.color;
+                c -= cl * Time.deltaTime * f;
+                m.color = c;
+                break;
+
+            case 1:
+                var rc = r.color;
+                rc.a = t * 3;
+                r.color = rc;
+                break;
+            default:
+                break;
+        }
+        
+
+        
 
         if (t <= 0)
         {
@@ -33,26 +53,40 @@ public class StoneLight : MonoBehaviour
 
     public void InitW()
     {
-        //m = GetComponent<Renderer>().material;
-        //m.color = Color.white * 2;
+        m = GetComponent<Renderer>().material;
+        m.color = Color.white * 2;
 
-        //cl = Color.white;
-
-        //f = 2;
+        cl = Color.white;
 
         r = transform.Find("StoneFire").GetComponent<Renderer>().material;
-        r.color = Color.white * 1.5f;
+        r.color = Color.black * 0;
+
+        f = 2;
+
+        kind = 0;
     }
 
     public void InitB()
     {
-        //m = GetComponent<Renderer>().material;
-        //cl = new Color(0.529f, 0, 0.875f);
-        //m.color = cl * 2;
+        m = GetComponent<Renderer>().material;
+        cl = new Color(0.529f, 0, 0.875f);
+        m.color = cl * 2;
 
-        //f = 4;
+        r = transform.Find("StoneFire").GetComponent<Renderer>().material;
+        r.color = Color.black * 0;
+
+        f = 4;
+
+        kind = 0;
+    }
+
+    public void InitS()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
 
         r = transform.Find("StoneFire").GetComponent<Renderer>().material;
         r.color = Color.white * 1.5f;
+
+        kind = 1;
     }
 }
