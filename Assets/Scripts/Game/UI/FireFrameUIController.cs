abd3066ee3ba9fea0c1fe7bb5fd2fc943e5c16e0
K_Ina <zuikaku1939y@gmail.com>
2024-11-29 15:20:07 +0900

    いらない

--- Assets/Scripts/Game/UI/FireFrameUIController.cs
+++ Assets/Scripts/Game/UI/FireFrameUIController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
