a183b1a491010d8bf6f114c0a71f22abbd6cbe4a
K_Ina <zuikaku1939y@gmail.com>
2024-11-29 15:17:42 +0900

    コンボボーナス

--- Assets/Scripts/Game/UI/FireFrameUIController.cs
+++ Assets/Scripts/Game/UI/FireFrameUIController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -9,14 +12,14 @@ public class FireFrameUIController : MonoBehaviour
     private RawImage img;
     private VideoPlayer ply;
 
-    float colorDelta = 0;
-
     int kind = 0;
     int count = 0;
 
     Sprite[] Sprites;
     Image comboImage;
     int currentIndex = 0;
+
+    TextMeshProUGUI tx;
 
     int[] comboCount = new int[]
     {
@@ -40,6 +43,8 @@ public class FireFrameUIController : MonoBehaviour
 
     public void Init()
     {
+        tx = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+
         img = GetComponent<RawImage>();
         img.color = new Color(1, 1, 1);
 
@@ -84,6 +89,9 @@ public class FireFrameUIController : MonoBehaviour
             case 4:
                 ply.clip = Resources.Load<VideoClip>("Movie/FireRainbow");
                 break;
+
+            default:
+                break;
         }
         
     }
@@ -103,8 +111,43 @@ public class FireFrameUIController : MonoBehaviour
     /// <returns></returns>
     public int GetCurrentState()
     {
-        return kind;
+        switch (kind)
+        {
+            default:
+            case 0:
+                return 0;
+
+            case 1:
+                return 1;
+
+            case 2:
+                return 2;
+
+            case 3:
+                return 3;
+
+            case 4:
+                return 5;
+        }
     }
 
+    public void ShowComboEnter()
+    {
+        transform.Find("ComboEnter").gameObject.SetActive(true);
 
+        var e = DelayMethod(() => { Destroy(transform.Find("ComboEnter").gameObject); }, 4.5f);
+        StartCoroutine(e);
+    }
+
+    public void SetText(string text)
+    {
+        tx.text = text;
+    }
+
+    private IEnumerator DelayMethod(Action act, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        act();
+        yield break;
+    }
 }
