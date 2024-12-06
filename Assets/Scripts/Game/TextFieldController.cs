06ae14b90946f12c5165b1e40af1352a016d9a36
K_Ina <zuikaku1939y@gmail.com>
2024-12-06 14:27:36 +0900

    会話用スクリプト

--- Assets/Scripts/Game/TextFieldController.cs
+++ Assets/Scripts/Game/TextFieldController.cs
@@ -4,11 +4,23 @@ using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// TextFieldControllerの選択肢用
+/// </summary>
+public struct TextSelectItem
+{
+    public string Text;
+    public int Index;
+}
 
 public class TextFieldController : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI TextObject;
+
+    Transform selectTarget;
 
     List<Action> actions = new List<Action>();
     int index = 0;
@@ -16,19 +28,27 @@ public class TextFieldController : MonoBehaviour
 
     bool isPressAnyKey = false;
 
+    bool isSelect = false;
+    List<TextSelectItem> selects = new List<TextSelectItem>();
+    int currentSelectIndex = 0;
+
     List<Func<bool>> fWaits = new List<Func<bool>>();
     int fWaitID = -1;
+
+    List<Func<int>> fJumps= new List<Func<int>>();
+    int fJumpID = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        TextObject.text = "";
 
+
+        //テスト用
         fWaits.Add(() => { return (Input.GetKeyDown(KeyCode.Space)); });
 
-        Init(Path.Combine(Application.streamingAssetsPath, "TextTest.txt"));
 
-        actions[index]();
+        //テスト用
+        Init(Path.Combine(Application.streamingAssetsPath, "TextTest.txt"));
     }
 
     // Update is called once per frame
@@ -61,10 +81,19 @@ public class TextFieldController : MonoBehaviour
                 fWaitID = -1;
             }
         }
+
+        if (fJumpID >= 0)
+        {
+            index = fJumps[fJumpID]();
+            fJumpID = -1;
+        }
     }
 
     public void Init(string filePath)
     {
+        TextObject.text = "";
+        selectTarget = GameObject.Find("Canvas").transform.Find("Select");
+
         index = 0;
         actions.Clear();
         using(System.IO.StreamReader sr = new System.IO.StreamReader(filePath))
@@ -85,6 +114,8 @@ public class TextFieldController : MonoBehaviour
             }
         }
 
+        actions[index]();
+
     }
 
     private IEnumerator DelayMethod(Action action, float seconds)
@@ -94,6 +125,11 @@ public class TextFieldController : MonoBehaviour
         yield break;
     }
 
+    /// <summary>
+    /// コマンドを挿入する
+    /// </summary>
+    /// <param name="commands">line.Split(' ')をした戻り値</param>
+    /// <returns></returns>
     private Action GetCommand(string[] commands)
     {
         switch(commands[0])
@@ -106,8 +142,46 @@ public class TextFieldController : MonoBehaviour
                 return () => { isPressAnyKey = true; };
 
             case "!fWait":
-                int id = int.Parse(commands[1]);
-                return () => { fWaitID = id; };
+                int idw = int.Parse(commands[1]);
+                return () => { fWaitID = idw; };
+
+            case "!jump":
+                int idx = int.Parse(commands[1]) - 1;
+                return () => { index = idx; };
+
+            case "!fJump":
+                int idj = int.Parse(commands[1]);
+                return () => { fJumpID = idj; };
+
+            case "!select":
+                return () =>
+                {
+                    selects.Clear();
+                    selects.Add(new TextSelectItem { Text = commands[1], Index = int.Parse(commands[2]) - 1 });
+                    selects.Add(new TextSelectItem { Text = commands[3], Index = int.Parse(commands[4]) - 1 });
+                    var obj = Resources.Load<GameObject>("UI/Prefab/TextButton");
+                    foreach (var item in selects)
+                    {
+                        var s = GameObject.Instantiate(obj);
+                        s.transform.SetParent(selectTarget, false);
+                        var btn = s.transform.Find("B").GetComponent<Button>();
+                        int idx = item.Index;
+                        string str = item.Text;
+                        s.transform.Find("B/TX").GetComponent<TextMeshProUGUI>().text = str;
+                        btn.onClick.AddListener(() => 
+                        { 
+                            index = idx; 
+                            for (int i = selectTarget.childCount - 1; i >= 0; i--) 
+                            { 
+                                Destroy(selectTarget.GetChild(i).gameObject);
+                            }
+                        });
+                    }
+
+                    isSelect = true;
+                };
+
+
 
             default:
                 return () => { index++; };
